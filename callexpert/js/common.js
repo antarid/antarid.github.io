73af8f3113e448eb43de49079066e79e2ec1f482
antarid <tarand007@gmail.com>
2018-06-13 13:53:15 +0300

    renamed

---
+++ callexpert/js/common.js
@@ -0,0 +1,78 @@
+$.fn.extend({
+	animateCss: function (animationName, callback) {
+		var animationEnd = (function (el) {
+			var animations = {
+				animation: 'animationend',
+				OAnimation: 'oAnimationEnd',
+				MozAnimation: 'mozAnimationEnd',
+				WebkitAnimation: 'webkitAnimationEnd',
+			};
+
+			for (var t in animations) {
+				if (el.style[t] !== undefined) {
+					return animations[t];
+				}
+			}
+		})(document.createElement('div'));
+
+		this.addClass('animated ' + animationName).one(animationEnd, function () {
+			$(this).removeClass('animated ' + animationName);
+
+			if (typeof callback === 'function') callback();
+		});
+
+		return this;
+	},
+});
+
+function animate() {
+	$('.owl-item.active .animate').each(function (element) {
+		const animation = $(this).attr('data-anim');
+		$(this).animateCss(animation);
+	})
+}
+
+$(function () {
+
+	$('.owl-carousel-main').owlCarousel({
+		dots: false,
+		loop: true,
+		margin: 0,
+		nav: true,
+		items: 1,
+		onTranslated: animate,
+	})
+
+	$('.owl-carousel-feedbacks').owlCarousel({
+		dots: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2,
+			},
+			1200: {
+				items: 3
+			}
+		}
+
+	})
+
+	$('.owl-item.active .animate').each(function (element) {
+		const animation = $(this).attr('data-anim');
+		$(this).animateCss(animation);
+	})
+
+	const width = $('.feedback .logo').width();
+	$('.feedback .logo').height(width)
+	console.log(width);
+
+	$('.feedbacks p').moreLines({
+		linecount: 3,
+		buttontxtmore: '<i style="margin:auto;" class="fa fa-arrow-down"></i>',
+		buttontxtless: '<i style="margin:auto;" class="fa fa-arrow-up"></i>'
+	});
+
+
+});
