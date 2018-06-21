cf497b4be0c5f6c284dd8a59ae5e919ab9e15a8c
antarid <tarand007@gmail.com>
2018-06-21 19:34:09 +0300

    mobile left

--- callexpert/js/common.js
+++ callexpert/js/common.js
@@ -1,3 +1,39 @@
+$.fn.read_more = function (opts) {
+	opts = opts || {};
+	var textContent = opts.text || 'Číst více',
+		hideContent = opts.text || 'Skrýt',
+		linkClass = opts.class || 'read-more-link';
+
+	return this.each(function (index, el) {
+		var $el = $(el),
+			$parent = $el.parent(),
+			$link = $('<a href="#read-more">' + textContent + '</a>');
+
+		textContent = $el.data('text') ? $el.data('text') : textContent;
+		hideContent = $el.data('hide-text') ? $el.data('hide-text') : hideContent;
+
+		$link.addClass(linkClass);
+		$el.hide().addClass('is-hidden');
+		$parent.append($link);
+
+		$link.on('click', function (event) {
+			event.preventDefault();
+
+			if ($el.hasClass('is-hidden')) {
+				$el.show()
+					.removeClass('is-hidden');
+				$link.text(hideContent);
+			} else {
+				$el.hide()
+					.addClass('is-hidden');
+				$link.text(textContent);
+			}
+
+		});
+	});
+
+};
+
 $.fn.extend({
 	animateCss: function (animationName, callback) {
 		var animationEnd = (function (el) {
@@ -32,9 +68,52 @@ function animate() {
 	})
 }
 
+function onScroll(event) {
+
+	var scrollPosition = $(document).scrollTop();
+	if (scrollPosition > 100)
+		$('header').addClass('moved');
+	else
+		$('header').removeClass('moved');
+
+	$('nav a').each(function () {
+		var currentLink = $(this);
+		var refElement = $(currentLink.attr("href"));
+		if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+			$('nav ul li a').removeClass("active");
+			currentLink.addClass("active");
+		}
+		else {
+			currentLink.removeClass("active");
+		}
+	});
+}
+
 $(function () {
 
+	$(document).on("scroll", onScroll);
+
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+
+		$('a').each(function () {
+			$(this).removeClass('active');
+		})
+		$(this).addClass('active');
+
+		var target = this.hash;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top + 2
+		}, 500, 'swing', function () {
+			window.location.hash = target;
+			$(document).on("scroll", onScroll);
+		});
+	});
+
 	$('.owl-carousel-main').owlCarousel({
+		autoplay: true,
 		dots: false,
 		loop: true,
 		margin: 0,
@@ -58,16 +137,32 @@ $(function () {
 		}
 
 	})
+
+
+	const navTitles = [];
 	$('.steps-carousel').owlCarousel({
+		autoplay: true,
 		nav: true,
-		dots: true,
 		loop: true,
+		dots: true,
 		items: 1,
-		onInitialized: function () {
-			document.querySelectorAll('.steps-carousel .owl-dot span').forEach(function (element, index) {
-				element.innerHTML = index + 1;
+		onInitialize: function () {
+			document.querySelectorAll('.step h2').forEach(el => {
+				const title = el.attributes['data-nav-title'].value;
+				navTitles.push(title);
 			});
+		},
+		onInitialized: function () {
+			let maxHeight = 0;
+			document.querySelectorAll('.step .row').forEach(el => { maxHeight = Math.max(maxHeight, el.clientHeight) });
+			document.querySelectorAll('.step .row').forEach(el => { el.style.height = maxHeight + 'px' });
+			const navs = document.querySelectorAll('.steps-carousel .owl-dot span');
+			navs.forEach((nav, index) => nav.innerHTML = navTitles[index]);
 		}
+	});
+
+	$('.steps-carousel').on('mousedown', function () {
+		$('.steps-carousel').trigger('stop.owl.autoplay');
 	});
 
 	$('.owl-item.active .animate').each(function (element) {
@@ -78,14 +173,14 @@ $(function () {
 
 	const width = $('.feedback .logo').width();
 	$('.feedback .logo').height(width)
-	console.log(width);
 
-	$('.feedbacks p').moreLines({
-		animationspeed: 250,
-		linecount: 3,
-		buttontxtmore: '<i style="margin:auto;" class="fa fa-arrow-down"></i>',
-		buttontxtless: '<i style="margin:auto;" class="fa fa-arrow-up"></i>'
+	$('p.review').each(function () {
+		$(this).moreLines({
+			animationspeed: 250,
+			linecount: 4,
+			buttontxtmore: '<i style="margin:auto;" class="fa fa-arrow-down"></i>',
+			buttontxtless: '<i style="margin:auto;" class="fa fa-arrow-up"></i>'
+		});
 	});
-
 
 });
